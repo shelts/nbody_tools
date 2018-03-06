b0917d3f273bed8e44ed3e20cfcf28cd9d10a258
shelts <shelts@rpi.edu>
2018-03-05 19:55:40 -0500

    update

--- sweep_2D.py
+++ sweep_2D.py
@@ -43,49 +43,49 @@ run_r  = n
 run_rr = n
 run_m  = n
 run_mr = n
-which_sweeps = [run_ft, run_r, run_rr, run_m, run_mr]
+which_sweeps1 = [run_ft, run_r, run_rr, run_m, run_mr]
+which_sweeps2 = [run_ft, run_r, run_rr, run_m, run_mr]
 #--------------------------------------------------------------------------------------------------
 
     
 class sweep:
-    def __init__(self, parameter, nbody):
+    def __init__(self, p1, p2, nbody):
         os.system("mkdir " + path + "like_surface/hists/parameter_sweeps")
         
-        self.parameter = parameter #parameter index
-        self.data_vals = []
-        self.data_val_file = path + "like_surface/hists/parameter_sweeps" + "/" + parameters_names[self.parameter] + "_vals.txt"
+        self.p1 = p1 #parameter index
+        self.p2 = p2
+        self.data_vals1 = []
+        self.data_vals2 = []
+        self.data_val_file = path + "like_surface/hists/parameter_sweeps" + "/" + parameters_names[self.p1] + "_" + parameters_names[self.p2] + "_vals.txt"
         
         self.get_data_vals()
         self.run_sweep(nbody)
         self.write_data_vals()
         
     def get_data_vals(self):
-        if(random_iter):
-            self.data_vals.append(args[self.parameter]) #correct value
-        else:
-            val = ranges[self.parameter][0] # lower range
-            dN = (ranges[self.parameter][1] - ranges[self.parameter][0]) / search_N[self.parameter]
-            self.data_vals.append(val)
+        self.data_vals1.append(args[self.p1]) #correct value
+        self.data_vals2.append(args[self.p2])    
+        
+        for i in range(0, search_N[self.p1] * search_N[self.p2]):
+            val1 = random.uniform(0.0, 1.0) * (ranges[self.p1][1] - ranges[self.p1][0]) + ranges[self.p1][0]
+            val2 = random.uniform(0.0, 1.0) * (ranges[self.p2][1] - ranges[self.p2][0]) + ranges[self.p2][0]
+            self.data_vals1.append(val1)
+            self.data_vals2.append(val2)
             
-        for i in range(0, search_N[self.parameter]):
-            if(random_iter):
-                val = random.uniform(0.0, 1.0) * (ranges[self.parameter][1] - ranges[self.parameter][0]) + ranges[self.parameter][0]
-            else:
-                val += dN
-            self.data_vals.append(val)
-
     def run_sweep(self, nbody):
         paras = list(args)
-        for i in range(0, search_N[self.parameter]):
-            paras[self.parameter] = self.data_vals[i]
-            output_hist = folder + parameters_names[self.parameter] + "_hists/" + "arg_" + str(paras[0]) + "_" + str(paras[1]) + "_" + str(paras[2]) + "_" + str(paras[3]) + "_" + str(paras[4])
-            pipe_name = folder + "parameter_sweeps/" + parameters_names[self.parameter] + ".txt"
+        for i in range(0, search_N[self.p1] * search_N[self.p2]):
+            paras[self.p1] = self.data_vals1[i]
+            paras[self.p2] = self.data_vals2[i]
+            
+            output_hist = folder + parameters_names[self.p1] + "_" + parameters_names[self.p2] + "_hists/" + "arg_" + str(paras[0]) + "_" + str(paras[1]) + "_" + str(paras[2]) + "_" + str(paras[3]) + "_" + str(paras[4])
+            pipe_name = folder + "parameter_sweeps/" + parameters_names[self.p1] + "_" + parameters_names[self.p2] + ".txt"
             nbody.run(paras, output_hist, input_hist, pipe_name)
     
     def write_data_vals(self):
         f = open(self.data_val_file, 'w')
         for i in range(0, len(self.data_vals)):
-            f.write("%0.15f\n" % self.data_vals[i])
+            f.write("%0.15f\t%0.15f\n" % (self.data_vals1[i], self.data_vals2[i]))
         f.close()
         
 def mk_dirs():
@@ -107,10 +107,11 @@ def main():
     if(make_correct_hist):
         nbody.run(args, input_hist)
     
-    for i in range(0, len(which_sweeps)):
-        if(which_sweeps[i]):
-            sweeper = sweep(i, nbody)
-            del sweeper
+    for i in range(0, len(which_sweeps1)):
+        for j in range(0, len(which_sweeps2)):
+            if(which_sweeps1[i] and which_sweeps2[j]):
+                sweeper = sweep(i, j, nbody)
+                del sweeper
             
             
     return 0
